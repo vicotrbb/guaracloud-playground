8f19f6c7c375a23170683cc5f8495f60aa922733
Victor Bona <victor.bona@hotmail.com>
2026-04-03 01:21:46 -0300

    Update index.js

--- express/index.js
+++ express/index.js
@@ -3,6 +3,8 @@ const app = express();
 const port = process.env.PORT || 8080;
 const message = process.env.CUSTOM_MESSAGE ?? 'Express!'
 
+console.log(`Starting Service: ${process.env.SERVICE_CALLER ?? 'Express'}`);
+
 app.get("/", (req, res) => {
   res.json({ message: `Hello from ${message}` });
 });
