6ae45c993287fece9bb06011ff9f62ce16d261f2
Victor Bona <victor.bona@hotmail.com>
2026-04-09 17:51:06 -0300

    Update server.js

--- catalog-integration-backend/src/server.js
+++ catalog-integration-backend/src/server.js
@@ -7,12 +7,16 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.enableCors({
-  origin: ['*'],
-  methods: ['*'],
-  allowedHeaders: ['*'],
-  credentials: false,
-  maxAge: 86400,
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+  if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+  }
+
+  next();
 });
 
 const state = {
