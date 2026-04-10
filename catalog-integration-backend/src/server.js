bf510efd772390f790ffb0c0db184c8bb55e6fd8
vicotrbb <victor.bona@hotmail.com>
2026-04-10 16:31:54 -0300

    feat: add CORS to backend and static server to frontend for GuaraCloud catalog testing

--- catalog-integration-backend/src/server.js
+++ catalog-integration-backend/src/server.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import pg from "pg";
 import Redis from "ioredis";
@@ -6,6 +7,7 @@ import { connect } from "nats";
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
