2c7355ec162e7143eb09efa55f6b0efa034787de
Victor Bona <victor.bona@hotmail.com>
2026-05-08 13:12:44 -0300

    Update Program.cs

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -4,6 +4,6 @@ var app = builder.Build();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
-app.MapGet("/", () => new { message = "Hello from .NET!" });
+app.MapGet("/", () => new { message = "Hello from .NET 1!" });
 
 app.Run();
