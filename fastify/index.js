403c41446d433124293278706c66d7bf24fa6263
Victor Bona <victor.bona@hotmail.com>
2026-05-08 12:20:50 -0300

    Update index.js

--- fastify/index.js
+++ fastify/index.js
@@ -2,7 +2,7 @@ const fastify = require("fastify")({ logger: true });
 const port = process.env.PORT || 8080;
 
 fastify.get("/", async () => {
-  return { message: "Hello from Fastify!" };
+  return { message: "Hello from Fastify 1!" };
 });
 
 fastify.listen({ port: Number(port), host: "0.0.0.0" }).then(() => {
