c38d22dd982115754512eade3c4d1165d034cde6
Victor Bona <victor.bona@hotmail.com>
2026-05-12 01:00:20 -0300

    Update app.controller.ts

--- nestjs/src/app.controller.ts
+++ nestjs/src/app.controller.ts
@@ -4,6 +4,6 @@ import { Controller, Get } from "@nestjs/common";
 export class AppController {
   @Get()
   hello() {
-    return { message: "Hello from NestJS 12!" };
+    return { message: "Hello from NestJS 13!" };
   }
 }
